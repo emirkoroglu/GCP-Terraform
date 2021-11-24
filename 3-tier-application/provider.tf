63b60c6aad451e65337d69921d5082540e549db6
emirsway <emirmails@gmail.com>
2021-11-24 11:03:19 +0000

    mysql deletion protection set false

--- 3-tier-application/provider.tf
+++ 3-tier-application/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  // your project id goes to project 
+  // enter your project id to project
   project = "terrafom-333108"
-  region  = "us-central1"
+  region  = var.vm_config["region"]
+  zone = var.vm_config["zone"]
 }
-
--- 3-tier-application/sqldb.tf
+++ 3-tier-application/sqldb.tf
@@ -1,7 +1,9 @@
 resource "google_sql_database_instance" "master" {
-  name             = "master-instance"
-  database_version = "MYSQL_8_0"
-  region           = "us-central1"
+  name                = "master-instance"
+  database_version    = "MYSQL_8_0"
+  region              = "us-central1"
+  deletion_protection = "false" 
+  // if deletion_protection set true terraform destroy will not delete the instance
 
   settings {
     # Second-generation instance tiers are based on the machine
